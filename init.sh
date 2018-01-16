1bb9e92d29c0766ca8e1a13b04758c57425611c7
Alejandro Ramirez <armzprz@gmail.com>
2018-01-16 16:39:27 -0600

    Several additions
    - Update databases before installing
    - Install oh-my-zsh
    - Remove previous config files before anything is installed
    - Install latest node version

--- init.sh
+++ init.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-sudo apt install zsh silversearcher-ag tmux
+sudo apt update
+sudo apt install zsh silversearcher-ag tmux screenfetch
 chsh -s $(which zsh)
+
+# oh-my-zsh
+# A delightful community-driven (with 1,000+ contributors) framework for managing your zsh configuration. 
+# http://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Base16 colorscheme for shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# Remove configurations in case they existed
+rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmxu ~/.tmux.conf 2> /dev/null
 
 # Latest version of Vim
 sh scripts/vim-install.sh
@@ -13,12 +22,11 @@ sh scripts/vim-install.sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Remove configurations in case they existed
-rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmxu ~/.tmux.conf 2> /dev/null
-
 # Link new config files
 ln -s ~/dotfiles/files/zshrc ~/.zshrc
+ln -s ~/dotfiles/files/zsh_aliases ~/.zsh_aliases
 ln -s ~/dotfiles/files/vimrc ~/.vimrc
+ln -s ~/dotfiles/files/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/files/liquidpromptrc ~/.liquidpromptrc 
 ln -s ~/dotfiles/files/tmux.conf ~/.tmux.conf
 # TODO: Source function to update
@@ -29,6 +37,7 @@ vim +PlugInstall +qall
 # nvm
 # Node Version Manager - Simple bash script to manage multiple active node.js versions 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+nvm install node
 
 # fzf: A command-line fuzzy finder
 # https://github.com/junegunn/fzf
@@ -48,7 +57,7 @@ git clone https://github.com/nojhan/liquidprompt.git ~/.liquidprompt
 ln -s ~/dotfiles/files/liquid.theme ~/.liquidprompt/liquid.theme
 ln -s ~/dotfiles/files/prompt.ps1 ~/.liquidprompt/prompt.ps1
 
-echo "Finally, run the following to get liquidprompt going:
-source liquidprompt/liquidprompt
-source ~/.liquidpromptrc
-"
+echo
+echo
+echo "Set up completed. Restart the shell to start rocking this."
+screenfetch
